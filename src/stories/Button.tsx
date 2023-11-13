16263ff682a1ce28d837b6092af0c2784f06b14f
DEVFATH <ggje33@gmail.com>
2023-11-13 12:32:03 +0900

    tailwind css 속성 적용

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './button.css';
+import classNames from 'classnames';
 
 interface ButtonProps {
   /**
@@ -27,7 +28,7 @@ interface ButtonProps {
    */
   disabled?: boolean;
 }
-
+1;
 /**
  * Primary UI component for user interaction
  */
@@ -39,23 +40,22 @@ export const Button = ({
   disabled,
   ...props
 }: ButtonProps) => {
-  const mode = disabled
-    ? 'storybook-button--disabled'
-    : primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      // className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={classNames('storybook-button', {
+        'text-grayscale-500 bg-grayscale-300 border-1 border-grayscale-300 border-solid': disabled,
+        'text-grayscale-900 bg-transparent border-1 border-grayscale-900 border-solid':
+          !disabled && !primary,
+        'bg-primary-900 text-white': !disabled && primary,
+        'text-[12px] py-[10px] px-[16px]': size === 'small',
+        'text-[14px] py-[11px] px-[20px]': size === 'medium',
+        'text-[16px] py-[12px] px-[24px]': size === 'large',
+      })}
       onClick={props.onClick}
     >
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
     </button>
   );
 };
